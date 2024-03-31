9291d72672ae1831992aa068ae33808bd0748983
Macaroon <macaroon@macaroons-air.mynetworksettings.com>
2024-03-30 21:32:52 -0400

    rollback num_epochs

--- src/train_mlp.cu
+++ src/train_mlp.cu
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
     int hidden_dim = 16;
     int n_layers = 2;
     int batch_size = 32;
-    int num_epochs = 2;
+    int num_epochs = 10;
 
     for (;;)
     {
