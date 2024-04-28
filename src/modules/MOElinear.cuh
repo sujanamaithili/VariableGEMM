e6ef9b5b4b139f37d3a2dbb09a58b1e8f0c1d2ea
Nikhil <nikhil@nikhils-air.mynetworksettings.com>
2024-04-28 19:11:03 -0400

    integrate batched_gemm

--- src/modules/MOElinear.cuh
+++ src/modules/MOElinear.cuh
@@ -1,44 +1,68 @@
 #pragma once
-#include <vector>
+#include "modules/linear.cuh"
 #include "modules/param.cuh"
 #include "ops/op_elemwise.cuh"
-#include "ops/op_reduction.cuh"
 #include "ops/op_mm.cuh"
-#include "modules/linear.cuh"
+#include "ops/op_reduction.cuh"
+#include <algorithm>
+#include <vector>
 
-template<typename T>
-class MOELinearLayer {
-    private:
-        std::vector<LinearLayer<T>> experts;
-        std::vector<int> batch_splits;
-        // std::vector<cudaStream_t> streams;
-        int in_dim;
-        int out_dim;
-    public:
-    MOELinearLayer() : in_dim(0), out_dim(0) {}
-    MOELinearLayer(int in_dim_, int out_dim_, std::vector<int>& batch_splits_, bool gpu): in_dim(in_dim_), out_dim(out_dim_), batch_splits(batch_splits_){
-        for(int i=0; i < batch_splits.size(); i++){
-            experts.emplace_back(in_dim, out_dim, gpu);
-        }
+template <typename T> class MOELinearLayer {
+private:
+  std::vector<LinearLayer<T>> experts;
+  std::vector<int> batch_splits;
+  // std::vector<cudaStream_t> streams;
+  int in_dim;
+  int out_dim;
+
+public:
+  MOELinearLayer() : in_dim(0), out_dim(0) {}
+  MOELinearLayer(int in_dim_, int out_dim_, std::vector<int> &batch_splits_,
+                 bool gpu)
+      : in_dim(in_dim_), out_dim(out_dim_), batch_splits(batch_splits_) {
+    for (int i = 0; i < batch_splits.size(); i++) {
+      experts.emplace_back(in_dim, out_dim, gpu);
     }
+  }
 
-    void init() {
-        for(auto& expert: experts){
-            expert.init_uniform();
-        }
+  void init() {
+    for (auto &expert : experts) {
+      expert.init_uniform();
     }
+  }
 
-    void forward(const Tensor<float> &x, Tensor<float> &y) {
-        assert(x.h == y.h && y.w == out_dim);
-        int start = 0;
-        for(int i=0; i<experts.size(); i++){
-            int batch_size = batch_splits[i];
-            Tensor<T> x_sub = x.slice(start, start + batch_size, 0, in_dim);
-            Tensor<T> y_sub = y.slice(start, start + batch_size, 0, out_dim);
-            experts[i].forward(x_sub, y_sub);
-            start += batch_size;
-        }
-      
+  void forward(const Tensor<T> &x, Tensor<T> &y) {
+    assert(x.h == y.h && y.w == out_dim);
+    int start = 0;
+    // read flag from args
+    bool flag = true;
+    if (flag) {
+      std::vector<Tensor<T>> x_batched;
+      std::vector<Tensor<T>> y_batched;
+      std::vector<Tensor<T>> w_batched;
+      std::vector<Tensor<T>> b_batched;
+      for (int i = 0; i < experts.size(); i++) {
+        int batch_size = batch_splits[i];
+        x_batched.push_back(x.slice(start, start + batch_size, 0, in_dim));
+        y_batched.push_back(y.slice(start, start + batch_size, 0, out_dim));
+        w_batched.push_back(experts[i].w.t);
+        b_batched.push_back(experst[i].b.t);
+        start += batch_size;
+      }
+      batched_gemm(x_batched, w_batched, y_batched);
+
+      for (int i = 0; i < y_batched.size(); i++) {
+        op_add(y_batched[i], b_batched[i], y_batched[i]);
+      }
+
+    } else {
+      for (int i = 0; i < experts.size(); i++) {
+        int batch_size = batch_splits[i];
+        Tensor<T> x_sub = x.slice(start, start + batch_size, 0, in_dim);
+        Tensor<T> y_sub = y.slice(start, start + batch_size, 0, out_dim);
+        experts[i].forward(x_sub, y_sub);
+        start += batch_size;
+      }
     }
-
+  }
 };
--- src/utils/tensor.cuh
+++ src/utils/tensor.cuh
@@ -1,49 +1,42 @@
 #pragma once
 #include <assert.h>
-#include <random>
 #include <memory>
+#include <random>
 #include <sstream>
 #include <string>
 
 #include "utils/assert.cuh"
 
-#define ISCLOSE_RELTOL 1e-6 // this would not work for precision lower than float
+#define ISCLOSE_RELTOL                                                         \
+  1e-6 // this would not work for precision lower than float
 #define ISCLOSE_ABSTOL 1e-6
 
-//Index is a MACRO that returns the element of t tensor at row, col coordinate
-#define Index(t, row, col) ((((t).rawp)[(t).offset + (row) * (t).stride_h + (col) * (t).stride_w]))
-//IndexOutofBound is a MACRO to test whether coordinate row,col is considered out of bounds for tensor "t"
+// Index is a MACRO that returns the element of t tensor at row, col coordinate
+#define Index(t, row, col)                                                     \
+  ((((t).rawp)[(t).offset + (row) * (t).stride_h + (col) * (t).stride_w]))
+// IndexOutofBound is a MACRO to test whether coordinate row,col is considered
+// out of bounds for tensor "t"
 #define IndexOutofBound(t, row, col) ((((row) >= (t).h) || ((col) >= (t).w)))
 
-template <typename T>
-struct cudaDeleter
-{
-  void operator()(T *p) const
-  {
-    if (p != nullptr)
-    {
+template <typename T> struct cudaDeleter {
+  void operator()(T *p) const {
+    if (p != nullptr) {
       // std::cout << "Free p=" << p << std::endl;
       cudaFree(p);
     }
   }
 };
 
-template <typename T>
-struct cpuDeleter
-{
-  void operator()(T *p) const
-  {
-    if (p != nullptr)
-    {
+template <typename T> struct cpuDeleter {
+  void operator()(T *p) const {
+    if (p != nullptr) {
       // std::cout << "Free p=" << p << std::endl;
       free(p);
     }
   }
 };
 
-template <typename T>
-class Tensor
-{
+template <typename T> class Tensor {
 public:
   int32_t h; // height
   int32_t w; // width
@@ -54,30 +47,28 @@ public:
   std::shared_ptr<T> ref; // refcounted pointer, for garbage collection use only
   bool on_device;
 
-  Tensor() : h(0), w(0), stride_h(0), stride_w(0), offset(0), rawp(nullptr), on_device(false)
-  {
+  Tensor()
+      : h(0), w(0), stride_h(0), stride_w(0), offset(0), rawp(nullptr),
+        on_device(false) {
     ref = std::shared_ptr<T>(rawp, cpuDeleter<T>());
   }
 
   Tensor(int32_t h_, int32_t w_, bool on_device_ = false)
-      : h(h_), w(w_), stride_h(w_), stride_w(1), offset(0), on_device(on_device_)
-  {
-    if (on_device_)
-    {
+      : h(h_), w(w_), stride_h(w_), stride_w(1), offset(0),
+        on_device(on_device_) {
+    if (on_device_) {
       cudaAssert(cudaMalloc(&rawp, sizeof(T) * h * w));
       // std::cout << "cudaMalloc p=" << rawp << std::endl;
       ref = std::shared_ptr<T>(rawp, cudaDeleter<T>());
-    }
-    else
-    {
+    } else {
       rawp = (T *)malloc(sizeof(T) * h * w);
-      //std::cout << "malloc p=" << rawp << " h=" << h << " w=" << w << std::endl;
+      // std::cout << "malloc p=" << rawp << " h=" << h << " w=" << w <<
+      // std::endl;
       ref = std::shared_ptr<T>(rawp, cpuDeleter<T>());
     }
   }
 
-  void toHost(Tensor<T> &out) const
-  {
+  void toHost(Tensor<T> &out) const {
     assert(!out.on_device);
     assert(h == out.h && w == out.w);
     if (!on_device) {
@@ -87,18 +78,17 @@ public:
     out.offset = offset;
     out.stride_h = stride_h;
     out.stride_w = stride_w;
-    cudaAssert(cudaMemcpy(out.rawp, rawp, h * w * sizeof(T), cudaMemcpyDeviceToHost));
+    cudaAssert(
+        cudaMemcpy(out.rawp, rawp, h * w * sizeof(T), cudaMemcpyDeviceToHost));
   }
 
-  Tensor<T> toHost() const
-  {
+  Tensor<T> toHost() const {
     Tensor<T> t{h, w};
     toHost(t);
     return t;
   }
 
-  void toDevice(Tensor<T> &out) const
-  {
+  void toDevice(Tensor<T> &out) const {
     assert(out.on_device);
     assert(h == out.h && w == out.w);
     if (on_device) {
@@ -108,18 +98,17 @@ public:
     out.offset = offset;
     out.stride_h = stride_h;
     out.stride_w = stride_w;
-    cudaAssert(cudaMemcpy(out.rawp, rawp, h * w * sizeof(T), cudaMemcpyHostToDevice));
+    cudaAssert(
+        cudaMemcpy(out.rawp, rawp, h * w * sizeof(T), cudaMemcpyHostToDevice));
   }
 
-  Tensor<T> toDevice() const
-  {
+  Tensor<T> toDevice() const {
     Tensor<T> t{h, w, true};
     toDevice(t);
     return t;
   }
 
-  Tensor<T> transpose() const
-  {
+  Tensor<T> transpose() const {
     Tensor<T> t{};
     t.w = h;
     t.stride_w = stride_h;
@@ -132,8 +121,7 @@ public:
     return t;
   }
 
-  Tensor<T> slice(int start_h, int end_h, int start_w, int end_w) const
-  {
+  Tensor<T> slice(int start_h, int end_h, int start_w, int end_w) const {
     Tensor<T> t{};
     assert(start_h < end_h && end_h <= h);
     assert(start_w < end_w && end_w <= w);
@@ -148,29 +136,20 @@ public:
     return t;
   }
 
-  std::string str() const
-  {
+  std::string str() const {
     Tensor<T> t{};
-    if (on_device)
-    {
+    if (on_device) {
       t = toHost();
-    }
-    else
-    {
+    } else {
       t = *this;
     }
     std::stringstream ss;
-    for (int i = 0; i < h; i++)
-    {
-      for (int j = 0; j < w; j++)
-      {
-        if (std::is_same_v<T, char> || std::is_same_v<T, unsigned char>)
-        {
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j < w; j++) {
+        if (std::is_same_v<T, char> || std::is_same_v<T, unsigned char>) {
           ss << (int)Index(t, i, j) << " ";
-        }
-        else
-        {
-         // std::cout << "haha " << Index(t, i, j) << std::endl;
+        } else {
+          // std::cout << "haha " << Index(t, i, j) << std::endl;
           ss << Index(t, i, j) << " ";
         }
         ss << "";
@@ -180,8 +159,7 @@ public:
     return ss.str();
   }
 
-  T mean() const 
-  {
+  T mean() const {
     assert(!on_device);
     T sum = 0;
     for (int i = 0; i < h; i++) {
@@ -189,11 +167,10 @@ public:
         sum += Index(*this, i, j);
       }
     }
-    return sum/(h*w);
+    return sum / (h * w);
   }
 
-  T range() const 
-  {
+  T range() const {
     assert(!on_device);
     T min, max;
     min = max = Index(*this, 0, 0);
@@ -207,6 +184,6 @@ public:
         }
       }
     }
-    return max-min;
+    return max - min;
   }
 };
